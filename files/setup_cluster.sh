abef21d5b6b5d72918631bfd7294fa0ab725ded0
Andreas Karlsson <andreas.karlsson@percona.com>
2025-07-15 19:30:46 +0200

    WIP2

--- files/setup_cluster.sh
+++ files/setup_cluster.sh
@@ -4,4 +4,11 @@ set -e
 
 psql -v ON_ERROR_STOP=on "$1" <<SQL
 CREATE EXTENSION pg_tde;
+
+SELECT pg_tde_add_global_key_provider_vault_v2('global', 'http://keyring:8200', 'kv', '/var/lib/postgresql/vault-token', NULL);
+
+SELECT pg_tde_create_key_using_global_key_provider('db-postgres-key', 'global');
+SELECT pg_tde_set_key_using_global_key_provider('db-postgres-key', 'global');
+
+ALTER DATABASE postgres SET default_table_access_method = 'tde_heap';
 SQL
