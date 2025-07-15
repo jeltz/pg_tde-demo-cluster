4aff70018261b283a4c2d17885bf0cc8b9393d4b
Andreas Karlsson <andreas.karlsson@percona.com>
2025-07-15 19:30:54 +0200

    Enable WAL encryption
    This makes sure the Patroni cluster is up and running with WAL
    ecnryption enabled plus a WAL arhcive in PgBackRest which stores
    the plaintext segments so a reocvery can be done properly.

--- files/setup_cluster.sh
+++ files/setup_cluster.sh
@@ -4,4 +4,13 @@ set -e
 
 psql -v ON_ERROR_STOP=on "$1" <<SQL
 CREATE EXTENSION pg_tde;
+
+SELECT pg_tde_add_global_key_provider_vault_v2('global', 'http://keyring:8200', 'kv', '/var/lib/postgresql/vault-token', NULL);
+
+SELECT pg_tde_create_key_using_global_key_provider('server-key', 'global');
+SELECT pg_tde_set_server_key_using_global_key_provider('server-key', 'global');
+
+ALTER SYSTEM SET pg_tde.wal_encrypt = on;
 SQL
+
+/lib/postgresql/17/bin/pg_ctl restart -D /var/lib/postgresql/patroni-17
